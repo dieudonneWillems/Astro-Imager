fafe5bf7287cc75e7f04b22e8b3b92a5f8126d8e
Dieudonné Willems <don_willems@mac.com>
2020-02-22 12:07:35 +0100

    Removed build files from repository

--- Build/Intermediates/Astro Imager.build/Debug/AstroImager.build/DerivedSources/AstroImager_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char AstroImagerVersionString[];
- extern const double AstroImagerVersionNumber;
-
- const unsigned char AstroImagerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:AstroImager  PROJECT:Astro Imager-1" "\n";
- const double AstroImagerVersionNumber __attribute__ ((used)) = (double)1.;
--- Build/Intermediates/Astro Imager.build/Release/AstroImagerPlugin.build/Objects-normal/x86_64/AstroImagerPlugin_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/wonco/Development/Astro\ Imager/Build/Intermediates/Astro\ Imager.build/Release/AstroImagerPlugin.build/DerivedSources/AstroImagerPlugin_vers.c
